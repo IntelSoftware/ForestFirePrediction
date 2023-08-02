1823e5cf9588d1d0256b4c4b55abba9125c6017a
Bob Chesebrough <bob.chesebrough@intel.com>
2023-08-01 23:24:07 -0700

    add labStep_0.sh

---
+++ labStep2_0.sh
@@ -0,0 +1,7 @@
+conda create -y -n PT intelpython3_full
+conda activate PT
+conda install -y -c conda-forge jupyterlab
+conda install -y -c anaconda seaborn pandas tqdm
+.conda/envs/PT/bin/pip install torch==1.13.0a0+git6c9b55e torchvision==0.14.1a0  intel_extension_for_pytorch==1.13.120+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
+
+
