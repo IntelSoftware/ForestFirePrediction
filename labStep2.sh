216b0e9500de6ed3fc22e8322e489aba597a9924
Bob Chesebrough <bob.chesebrough@intel.com>
2023-08-01 21:46:19 -0700

    tweak labStep.sh

--- labStep2.sh
+++ labStep2.sh
@@ -1,9 +1,9 @@
-conda create -n PT2 intelpython3_full
+conda create -y -n PT2 intelpython3_full
 conda activate PT2
-conda install -c conda-forge jupyterlab
-conda install -c anaconda seaborn pandas tqdm
+conda install -y -c conda-forge jupyterlab
+conda install -y -c anaconda seaborn pandas tqdm
 pip install torch==1.13.0a0+git6c9b55e torchvision==0.14.1a0  intel_extension_for_pytorch==1.13.120+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
-pip install -r requirements.txt
+pip install wandb plotext opencv-python albumentations matplotlib  tabulate
 python -m ipykernel install --user --name PT2 --display-name "PT2"
 
 
